830381bf8bcf36b0e07ba2b463a10fe315ab58bf
Matjaz Gregoric <mtyaka@gmail.com>
2010-11-09 12:58:39 +0100

    Add lein-clojars to dev dependencies.

--- project.clj
+++ project.clj
@@ -1,2 +1,3 @@
 (defproject lein-oneoff "0.0.1"
-  :description "Dependency management for one-off scripts.")
+  :description "Dependency management for one-off scripts."
+  :dev-dependencies [[lein-clojars "0.6.0"]])
