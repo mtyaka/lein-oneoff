07469ade69bc8b86ebd2c240f750da30107333cf
Matjaz Gregoric <mtyaka@gmail.com>
2011-03-13 22:23:53 +0100

    Set eval-in-leiningen to true in project.clj.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject lein-oneoff "0.1.1-SNAPSHOT"
   :description "Dependency management for one-off scripts."
-  :dev-dependencies [[lein-clojars "0.6.0"]])
+  :dev-dependencies [[lein-clojars "0.6.0"]]
+  :eval-in-leiningen true)
