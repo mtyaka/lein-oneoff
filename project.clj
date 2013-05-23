6ffbe6c21805f71a0b8717e121ee3fd40c901c74
Matjaz Gregoric <mtyaka@gmail.com>
2013-05-23 23:30:40 +0200

    Set :min-lein-version to 2.0.0.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject lein-oneoff "0.2.0"
   :description "Dependency management for one-off scripts."
   :url "https://github.com/mtyaka/lein-oneoff"
-  :eval-in-leiningen true
+  :eval-in :leiningen
+  :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"})
