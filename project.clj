962ea789308d1af76a8b660450644f7e87a0ce9b
Matjaz Gregoric <mtyaka@gmail.com>
2013-05-23 23:24:34 +0200

    Add :url to project.clj.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject lein-oneoff "0.2.0"
   :description "Dependency management for one-off scripts."
+  :url "https://github.com/mtyaka/lein-oneoff"
   :eval-in-leiningen true
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"})
