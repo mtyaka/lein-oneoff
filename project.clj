884876858e918a22d29d851653cc0b0244e3485e
Matjaz Gregoric <mtyaka@gmail.com>
2013-05-23 23:23:33 +0200

    Add :license to project.clj.

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject lein-oneoff "0.2.0"
   :description "Dependency management for one-off scripts."
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"})
